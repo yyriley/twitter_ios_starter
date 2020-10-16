7834d5a2643ac5002674445716f492226ab441e4
Yrua <yyr7@txstate.edu>
2020-10-16 03:16:33 -0500

    Added like and retweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -22,6 +22,12 @@ class HomeTableViewController: UITableViewController {
       tableView.refreshControl = myRefreshControl
     }
   
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    self.loadTweets()
+  }
+  
+  
   @objc func loadTweets(){
     numberOfTweets = 20
     
@@ -95,6 +101,11 @@ class HomeTableViewController: UITableViewController {
     if let imageData = data {
       cell.profileImageView.image = UIImage(data: imageData)
     }
+    
+    cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+    cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+    cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+    
     
     return cell
   }
